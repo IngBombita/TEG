c16978c8bd020eff28a41da796c10ae861676158
Antares26 <emiliocignetti@gmail.com>
2018-12-06 18:39:54 -0300

    Added Coordinates-Getting Mechanism

--- app/containers/Board/index.js
+++ app/containers/Board/index.js
@@ -11,13 +11,37 @@
 
 import React from 'react';
 import map from './src/map.png';
+import { runInThisContext } from 'vm';
 
 /* eslint-disable react/prefer-stateless-function */
-export default class HomePage extends React.PureComponent {
+export default class HomePage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      height: 0,
+      width: 0
+    }
+    this.imgRef = React.createRef();
+  }
+  onMapClick = (elem) => {
+    var x = elem.pageX;
+    var y = elem.pageY;
+    console.log("Mouse pos: (", x, ',', y, ')');
+  }
+
+  componentDidMount() {
+    
+  }
+  onLoad = () => {
+    console.log("hola vieja");
+    var img = this.imgRef.current;
+    
+    console.log(` Width: ${img.clientWidth} Height: ${img.clientHeight}`);
+  }
   render() {
     return (
-      <div>
-        <img src={map} alt="board" />
+      <div /*ref={this.imgRef}*/>
+        <img src={map} ref={this.imgRef} onLoad={this.onLoad} alt="board" onClick={this.onMapClick} />
       </div>
     );
   }
