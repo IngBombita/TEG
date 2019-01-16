c980c56f81d9fe8d6af06299c614366826dd71ad
Antares26 <emiliocignetti@gmail.com>
2019-01-16 18:53:37 -0300

    Bug fixed

--- server/application/Validators/Game/attackValidator.js
+++ server/application/Validators/Game/attackValidator.js
@@ -5,7 +5,7 @@ exports.run = async function validate(gameState) {
     gameState.action.from.province,
     gameState.action.to.province,
   );
-  if (!isAvaibleRoute) throw new Error('No es una ruta validad');
+  if (!isAvaibleRoute) throw new Error('No es una ruta valida');
   if (gameState.action.from.chips <= 1)
     throw new Error('El Atacante no tiene suficientes fichas');
 };
--- server/infrastructure/Adapters/Game/attackAdapter.js
+++ server/infrastructure/Adapters/Game/attackAdapter.js
@@ -1,7 +1,7 @@
 exports.attack = function attack(data) {
   if (!data.action)
     throw new Error('No se expecifico que ataque desea realizar');
-  if (data.roundNumer < 3)
+  if (data.roundNumber < 3)
     throw new Error('No se ha llegado a la etapa de ataques todavia');
   return data;
 };
